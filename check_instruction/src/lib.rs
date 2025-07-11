dc166adb5e12d235363875bbdb2a337ea0468e97
anurag bagri <anurag123bagri@gmail.com>
2025-07-11 14:46:11 +0530

    data_account added

--- check_instruction/src/lib.rs
+++ check_instruction/src/lib.rs
@@ -1,9 +1,13 @@
 use solana_program::{
     account_info::{AccountInfo, next_account_info},
     entrypoint::ProgramResult,
+    example_mocks::solana_sdk::system_instruction,
     msg,
+    program::invoke,
     program_error::ProgramError,
+    rent::Rent,
     system_program,
+    sysvar::Sysvar,
 };
 
 entrypoint!(process_instruction);
@@ -12,6 +16,7 @@ fn process_instruction(
     program_id: &Pubkey,
     accounts: &mut [AccountInfo],
     instruction: &[u8],
+    space: usize,
 ) -> ProgramResult {
     // first lets check that whether the program id from the instruction is program id of the our program or not
     if system_program::check_id(program_id) {
@@ -26,10 +31,10 @@ fn process_instruction(
 
     // Accounts passed in the vector must of the following order
     let account_iter = accounts.iter();
-    let payer = next_account_info(&account_iter)?;
-    let account_to_create = next_account_info(&account_iter)?;
-    let account_to_change = next_account_info(&account_iter)?;
-    let system_program = next_account_info(&account_iter)?;
+    let payer = next_account_info(account_iter)?;
+    let account_to_create = next_account_info(account_iter)?;
+    let account_to_change = next_account_info(account_iter)?;
+    let system_program = next_account_info(account_iter)?;
 
     // check accounttocreate is initialized or not
     msg!("account to create is {}", account_to_create.key);
@@ -38,7 +43,27 @@ fn process_instruction(
         return Err(ProgramError::AccountAlreadyInitialized);
     }
 
-    // (create account)
+    // (create account) if data account does not exists
+    let rent = Rent::get()?;
+    let lamports_req = rent.minimum_balance(space);
+
+    let create_tnx = system_instruction::create_account(
+        payer.key,
+        account_to_create.key,
+        lamports_req,
+        space as u64,
+        program_id,
+    );
+
+    // invoke system_program to create a data account on current program behalf(your program account/contract)
+    invoke(
+        &create_tnx,
+        &[
+            payer.clone(),
+            account_to_create.clone(),
+            system_program.clone(),
+        ],
+    )?;
 
     // make sure accounttochange has already been initialized
     msg!("account to change is {}", account_to_change.key);
