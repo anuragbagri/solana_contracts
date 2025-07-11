0d834ec9661d9d217b5ae76d384d44c07f8d10ef
anurag bagri <anurag123bagri@gmail.com>
2025-07-11 14:26:28 +0530

    check_instructions added

---
+++ check_instruction/src/lib.rs
@@ -0,0 +1,55 @@
+use solana_program::{
+    account_info::{AccountInfo, next_account_info},
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    system_program,
+};
+
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &mut [AccountInfo],
+    instruction: &[u8],
+) -> ProgramResult {
+    // first lets check that whether the program id from the instruction is program id of the our program or not
+    if system_program::check_id(program_id) {
+        return Err(ProgramError::IncorrectProgramId);
+    };
+
+    if accounts.len < 4 {
+        msg!("this accounts instruction array requires 4 accounts");
+        msg!(" payer , account_To_create , account_to_change , system_program");
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    // Accounts passed in the vector must of the following order
+    let account_iter = accounts.iter();
+    let payer = next_account_info(&account_iter)?;
+    let account_to_create = next_account_info(&account_iter)?;
+    let account_to_change = next_account_info(&account_iter)?;
+    let system_program = next_account_info(&account_iter)?;
+
+    // check accounttocreate is initialized or not
+    msg!("account to create is {}", account_to_create.key);
+    if account_to_create.lamports() != 0 {
+        msg!("account to create has already been initialized ");
+        return Err(ProgramError::AccountAlreadyInitialized);
+    }
+
+    // (create account)
+
+    // make sure accounttochange has already been initialized
+    msg!("account to change is {}", account_to_change.key);
+    if account_to_change.lamports() == 0 {
+        msg!("account not initialized");
+        return Err(ProgramError::UninitializedAccount);
+    };
+
+    // check account_to_change is owned by system program or not
+    if account_to_change.owner != program_id {
+        msg!("Account to change does not have the correct porgram id");
+        return Err(ProgrmaError::IncorrectProgramId);
+    }
+}
