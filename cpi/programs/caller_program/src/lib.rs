4c3bb07512924fcc729d20b27aae9dbdcb1e9eb7
anurag bagri <anurag123bagri@gmail.com>
2025-07-14 17:29:51 +0530

    prog-caller-init

--- cpi/programs/caller_program/src/lib.rs
+++ cpi/programs/caller_program/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// Program A: caller_program/src/lib.rs
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use solana_program::{
+    account_info::{AccountInfo, next_account_info},
+    entrypoint,
+    entrypoint::ProgramResult,
+    instruction::{AccountMeta, Instruction},
+    msg,
+    program::invoke,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("Program A: Invoking Program B");
+
+    let accounts_iter = &mut accounts.iter();
+    let caller = next_account_info(accounts_iter)?; // payer/signer
+    let counter_account = next_account_info(accounts_iter)?;
+    let program_b = next_account_info(accounts_iter)?; // Program B address
+
+    let ix = Instruction {
+        program_id: *program_b.key,
+        accounts: vec![
+            AccountMeta::new(*counter_account.key, false), // counter account
+        ],
+        data: vec![], // Program B doesn't expect any instruction data
+    };
+
+    // Perform CPI
+    invoke(&ix, &[counter_account.clone(), program_b.clone()])?;
+
+    Ok(())
 }
