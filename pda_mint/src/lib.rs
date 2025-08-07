31ddaa42aec16e046a2acbd9017f7022ff74c2e3
anurag bagri <anurag123bagri@gmail.com>
2025-08-07 16:39:51 +0530

    lib.rs compl

--- pda_mint/src/lib.rs
+++ pda_mint/src/lib.rs
@@ -1,3 +1,16 @@
 pub mod instructions;
 pub mod processor;
 pub mod state;
+
+
+
+use solana_program::{
+    account_info::{next_account_info,AccountInfo},
+    entrypoint::{self, ProgramResult},
+    msg,
+    pubkey::Pubkey
+};
+entrypoint!(process_instruction)
+fn process_instruction(program_id : &Pubkey , accounts : &[AccountInfo] , instruction_data  : &[u8]) -> ProgramResult {
+   processor::process_instruction(program_id, accounts, instruction_data);
+}
